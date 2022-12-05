15011a35bc73a99b227190d6d5b5ed11fd66df2e
PabloGarciaUH <pablo.garcia@bootcamp-upgrade.com>
2022-12-05 20:00:28 +0100

    Iteración #1

---
+++ functions.js
@@ -0,0 +1,13 @@
+// Iteración #1 //
+
+sum(27,10);
+
+function sum(numberOne , numberTwo) {
+    if(numberOne>=numberTwo) {
+        console.log(numberOne);
+    }
+    else {
+        console.log(numberTwo);
+    }
+}
+
