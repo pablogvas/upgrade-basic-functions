57b42dbb2192f1229312311f30b0a9ab69dd7e41
PabloGarciaUH <pablo.garcia@bootcamp-upgrade.com>
2022-12-08 20:05:13 +0100

    Iteración7

--- functions.js
+++ functions.js
@@ -105,3 +105,32 @@ function removeDuplicates(param) {
 
 removeDuplicates(duplicates);
 
+
+// Iteración #7 //
+
+const nameFinder = [
+    'Peter',
+    'Steve',
+    'Tony',
+    'Natasha',
+    'Clint',
+    'Logan',
+    'Xabier',
+    'Bruce',
+    'Peggy',
+    'Jessica',
+    'Marc'
+  ];
+function finderName(param, valor) {
+    let posicion = 0;
+    for(i=0; i<param.length; i++) {
+        if(param[i] === valor) {
+            console.log(true);
+            posicion += i;
+        }
+    }
+    console.log(posicion);
+}
+
+finderName(nameFinder, 'Xabier');
+
